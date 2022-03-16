206e0882931b196204dec0fc3e7c1af121e7e96c
MonaMaNotAvailable <zma4@wpi.edu>
2022-03-16 01:46:30 -0400

    make portfolio

---
+++ src/components/education.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Education extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="education">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Education</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Tufts University: Master of Science <span>Sep 2022 - May 2024</span></h2>
+                        <p>I am pursuing a graduate degree in computer science at Tufts, with concentration on machine learning, algorithms, and data analysis.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-6">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Worcester Polytechnic Institute (WPI): Bachelor of Science <span>Aug 2018 - Dec 2021</span></h2>
+                        <p>I double major in math and computer science. I am dedicated to international student community. I joined k-pop dance group, ultimate frisbee team, and choir. I became a member of Upsilon Pi Epsilon (computer science honor society) in my junior year. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Beijing 101 Middle School: International Department (101ID) <span>Sep 2015 - May 2018</span></h2>
+                        <p>I came from Beijing, China, and prepared to study abroad during high school by taking 7 AP courses, 5 TOEFL exams, and SAT for 3 times.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/foodmap.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Foodmap extends Component {
+    render() {
+      return (
+        <div>
+            <h1>To be continued...</h1>
+        </div>
+      )
+    }
+}
