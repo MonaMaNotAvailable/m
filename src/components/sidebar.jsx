8b95b607549667be2f35a6f7f5edfc294a3c7415
MonaMaNotAvailable <zma4@wpi.edu>
2022-03-16 02:19:41 -0400

    fix image path

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(../images/cover_version1.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(/images/cover_version1.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Zhifei Ma</a></h1>
               <span className="email"><i className="icon-mail"></i> zma4@wpi.edu</span>
             </div>
